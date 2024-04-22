c4c635ec6cd6e624dfa52904921480aab25c8753
Martin Stuwe <MartinStuwe@ProtonMail.com>
2024-04-22 13:07:18 +0200

    send position updates to ACT-R model

--- ingame_objects/comets.py
+++ ingame_objects/comets.py
@@ -1,20 +1,53 @@
 import pygame
 import os
 import random
+import  actr.rpc_interface
+import asyncio
+import threading
+import json
 
+loop = None
+
+global_id_counter = 0
+
+def start_async_loop():
+    global loop
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_forever()
+
+asyncio_thread = threading.Thread(target=start_async_loop, daemon=True)
+asyncio_thread.start()
+asyncio_thread.join(0.1)  # Give the thread some time to set up the loop
 
 class Comet(pygame.sprite.Sprite):
     def __init__(self, pos, size):
         super().__init__()
-        # actual comet image which will be drawn on rectangular surface of comet
         self.image = pygame.image.load(os.path.join('assets/comets', 'comet_master.png')).convert_alpha()
-        self.random_angle = random.choice([0, 90, 180, 270])  # items of list are random angles in 90 degree steps
-        self.image = pygame.transform.rotate(pygame.transform.scale(self.image, (size, size)), angle=self.random_angle)
+        self.random_angle = random.choice([0, 90, 180, 270])
+        self.image = pygame.transform.rotate(pygame.transform.scale(self.image, (size, size)), self.random_angle)
         self.rect = self.image.get_rect(topleft=pos)
+        self.last_sent = pygame.time.get_ticks()  # Initialize last_sent properly
+        self.update_freq = 1000  # Increase frequency if needed (ms)
+        self.id = global_id_counter + 1
+        global_id_counter += 1
 
     def update(self, speed, scaling, horizontal_movement):
-        # vertical movement
-        self.rect.y -= (1*scaling) * speed
-        # horizontal movement
-        self.rect.x += horizontal_movement * scaling * speed
+        self.rect.y -= int(1 * scaling * speed)
+        self.rect.x += int(horizontal_movement * scaling * speed)
+        current_time = pygame.time.get_ticks()
 
+        # comment out to see if still lagging
+        if current_time - self.last_sent > self.update_freq:
+            self.update_visicon()
+            self.last_sent = current_time
+
+    def update_visicon(self):
+        #message = f"{{\"method\": \"evaluate\", \"params\": [\"add-visicon-features\", \"compas-model\", [\"screen-x\", {self.rect.x}, \"screen-y\", {self.rect.y}], \"id\": 1}}"
+        message = f"{{\"method\": \"evaluate\", \"params\":[\"add-visicon-features\", \"compas-model\", [\"screen-x\", 2, \"screen-y\", 2]], \"id\": 1}}"
+        #message2 = '{"method": "evaluate", "params":["add-visicon-features", "compas-model", ["screen-x", 2, "screen-y", 2]], "id": 1}'
+        #print("message2 is: " + message2)
+        asyncio.run_coroutine_threadsafe(actr.rpc_interface.communicate(message=message), loop)
+
+
+    #await send(sock,'{"method": "evaluate", "params":["add-visicon-features", "compas-model", ["screen-x", 2, "screen-y", 2]], "id": 1}')
